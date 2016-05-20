a3b47f91f2a95c68b85c633609d00400bef09096
McNightmare <ricky.vandenberg@hotmail.nl>
2016-05-20 15:00:37 +0200

    added applicatie + db

---
+++ Applicatie/GGzBreburgApp/GGzBreburgApp/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using GGzBreburgApp.Resources;
+
+namespace GGzBreburgApp
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
